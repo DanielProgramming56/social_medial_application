a1d56530705e7c0afa95821daaa2edc834cb2b52
daniel.chidebere <daniel.chidebere@konga.com>
2023-12-31 18:00:36 +0100

    done

--- client/src/components/userImage.jsx
+++ client/src/components/userImage.jsx
@@ -8,10 +8,10 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3005/assets/${image}`}
+        src={`${process.env.REACT_APP_PORT}/assets/${image}`}
       />
     </Box>
-  );
+  )
 };
 
 export default UserImage;
